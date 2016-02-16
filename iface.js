96fe5be438bf7225024dc0e12f429dba1ffd0565
James Halliday <mail@substack.net>
2016-02-15 21:36:07 -0800

    rpc interface

---
+++ iface.js
@@ -0,0 +1,49 @@
+var EventEmitter = require('events').EventEmitter
+var minimist = require('minimist')
+var fs = require('fs')
+var has = require('has')
+var inherits = require('inherits')
+var level = require('level')
+var sodium = require('chloride')
+var swarmbot = require('./')
+
+var iface = null
+module.exports = function (server, stream, args) {
+  if (!iface) iface = new Iface(server, stream, args)
+  return iface
+}
+module.exports.prototype = Iface.prototype
+inherits(Iface, EventEmitter)
+
+function Iface (server, stream, args) {
+  if (!(this instanceof Iface)) return new Iface(server, stream, args)
+  EventEmitter.call(this)
+  var iface = new EventEmitter
+  var argv = minimist(args)
+  this.keys = require(argv.keys)
+  this.swarmbot = swarmbot({
+    logdb: level(path.join(argv.dir, 'log.db')),
+    idb: level(path.join(argv.dir, 'index.db')),
+    hubs: [ 'https://signalhub.mafintosh.com' ],
+    keys: this.keys,
+    sodium: sodium
+  })
+}
+
+Iface.prototype.id = function (cb) {
+  cb(null, this.keys.public)
+}
+
+Iface.prototype.mirror = function (id, cb) {
+  this.swarmbot.mirror(id, cb)
+}
+
+Iface.prototype.unmirror = function (id, cb) {
+  this.swarmbot.unmirror(id, cb)
+}
+
+Iface.prototype.mirroring = function (cb) {
+  this.swarmbot.mirroring(cb)
+}
+
+function noop () {}
